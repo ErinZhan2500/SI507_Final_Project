c6aba7d4592c33d018ec0a409817071b879e6483
ErinZhan2500 <45149517+ErinZhan2500@users.noreply.github.com>
2023-04-17 19:44:40 -0400

    Draft 2

--- FinalProject.py
+++ FinalProject.py
@@ -6,7 +6,6 @@
 
 import requests
 import json 
-import webbrowser
 
 CACHE_FILE = "usda_cache.json"
 CACHE_DICT = {}
@@ -131,8 +130,8 @@ def search_recipe(query, cuisine):
     return response
 
 
-recipe_data = search_recipe("Pasta", "Italian")
-print(recipe_data)
+#recipe_data = search_recipe("Pasta", "Italian")['results'][0:5]
+#print(recipe_data)
 
 #Walmart data
 def search_item(search_term):
@@ -151,38 +150,197 @@ def search_item(search_term):
 
 
 # print the JSON response from BlueCart API
-print(search_item("Apple"))
+#print(search_item("Apple"))
 
 #define the decision tree
 tree = {
     'question': 'What type of cuisine do you prefer?',
-    'answers': ['Italian', 'Asian', 'American', 'Mexican', 'Mediterranean'],
-    'nodes': "What's your"
-}
+    'answers': ['Italian', 'Asian', 'Mexican'],
+    'nodes':[
+        {
+            'question': 'What type of Italian dish would you like to cook?',
+            'answers': ['Pasta', 'Pizza', 'Risotto'],
+            'nodes': [
+                {
+                    'question': 'What type of pasta would you like to make?',
+                    'answers': ['Spaghetti', 'Lasagna', 'Fettuccine'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Spaghetti', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Lasagna', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Fettuccine', 'Italian')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of pizza would you like to make?',
+                    'answers': ['Margherita', 'Pepperoni', 'Vegetarian'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Margherita', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Pepperoni', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Vegetarian', 'Italian')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of risotto would you like to make?',
+                    'answers': ['Mushroom', 'Seafood', 'Asparagus'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Mushroom', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Seafood', 'Italian')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Asparagus', 'Italian')['results'][0:5]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'question': 'What type of Asian dish would you like to cook?',
+            'answers': ['Chinese', 'Japanese', 'Thai'],
+            'nodes': [
+                {
+                    'question': 'What type of Chinese dish would you like to make?',
+                    'answers': ['Stir Fry', 'Dumplings', 'Hot Pot'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Stir Fry', 'Chinese')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Dumplings', 'Chinese')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Hot Pot', 'Chinese')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of Japanese dish would you like to make?',
+                    'answers': ['Sushi', 'Ramen', 'Teriyaki'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Sushi', 'Japanese')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Ramen', 'Japanese')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Teriyaki', 'Japanese')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of Thai dish would you like to make?',
+                    'answers': ['Curry', 'Pad Thai', 'Tom Yum'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Curry', 'Thai')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Pad Thai', 'Thai')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Tom Yum', 'Thai')['results'][0:5]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            'question': 'What type of Mexican dish would you like to cook?',
+            'answers': ['Tacos', 'Enchiladas', 'Chiles Rellenos'],
+            'nodes': [
+                {
+                    'question': 'What type of filling would you like for your tacos?',
+                    'answers': ['Beef', 'Chicken', 'Vegetarian'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Beef Tacos', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Chicken Tacos', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Vegetarian Tacos', 'Mexican')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of sauce would you like for your enchiladas?',
+                    'answers': ['Red Sauce', 'Green Sauce', 'Mole Sauce'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Enchiladas with Red Sauce', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Enchiladas with Green Sauce', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Enchiladas with Mole Sauce', 'Mexican')['results'][0:5]
+                        }
+                    ]
+                },
+                {
+                    'question': 'What type of stuffing would you like for your chiles rellenos?',
+                    'answers': ['Cheese', 'Beef', 'Vegetables'],
+                    'nodes': [
+                        {
+                            'result': search_recipe('Cheese Stuffed Chiles Rellenos', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Beef Stuffed Chiles Rellenos', 'Mexican')['results'][0:5]
+                        },
+                        {
+                            'result': search_recipe('Vegetable Stuffed Chiles Rellenos', 'Mexican')['results'][0:5]
+                        }
+                    ]
+                }
+            ]
+        }]}
+    
 
 def decision_tree(node):
-    if isinstance(node, dict):
+    if isinstance(node, dict) and 'answers' in node:
+        print(node['answers'])
         answer = input(node['question'])
         while answer not in node['answers']:
             print("Invalid answer. Please try again.")
             answer = input(node['question'])
         next_node = node['nodes'][node['answers'].index(answer)]
-        decision_tree(next_node)
+        return decision_tree(next_node)
     else:
-        get_recipes()
+        return node
 
+def get_recipes():
+    results = decision_tree(tree)['results']
+    print_recipes(results)
 
-
-
-
-
+# Results in the form of a JSON dictionary
+def print_recipes(results):
+    print("Here are the matching recipes:\n")
+    print(json.dumps(results, indent=2))
 
 
 def main():
-
     print("Welcome to the Personalized Meal Plan Guide!")
-    root = ("What type of cuisine do you prefer?", None, None)
+    print("Answer the following questions to get a personalized meal plan.")
+    get_recipes()
 
+if __name__ == "__main__":
+    main()
 
 
 
